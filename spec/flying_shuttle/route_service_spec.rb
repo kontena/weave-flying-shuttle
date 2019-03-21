8473087f8f5bdd3d0b27d4d6fa0aff68c293c01e
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-03-21 09:49:04 +0200

    more specs

--- spec/flying_shuttle/route_service_spec.rb
+++ spec/flying_shuttle/route_service_spec.rb
@@ -14,4 +14,35 @@ RSpec.describe FlyingShuttle::RouteService do
       expect(addresses).to eq(['10.9.145.75', '10.9.145.76'])
     end
   end
+
+  describe "#update_routes" do
+    it "adds new routes" do
+      addresses = ['10.10.2.2', '192.168.10.10']
+      allow(subject).to receive(:addresses_needing_route).and_return(addresses)
+      allow(subject).to receive(:currently_routed_addresses).and_return([])
+      allow(subject).to receive(:ensure_route) do |addr|
+        expect(addresses.include?(addr)).to be_truthy
+      end
+      expect(subject).not_to receive(:remove_route)
+      subject.update_routes
+    end
+
+    it "adds only new route" do
+      addresses = ['10.10.2.2', '192.168.10.10']
+      allow(subject).to receive(:addresses_needing_route).and_return(addresses)
+      allow(subject).to receive(:currently_routed_addresses).and_return([addresses.last])
+      allow(subject).to receive(:ensure_route).with(addresses.first)
+      expect(subject).not_to receive(:remove_route)
+      subject.update_routes
+    end
+
+    it "does not do anything if all routes exist" do
+      addresses = ['10.10.2.2', '192.168.10.10']
+      allow(subject).to receive(:addresses_needing_route).and_return(addresses)
+      allow(subject).to receive(:currently_routed_addresses).and_return(addresses)
+      expect(subject).not_to receive(:ensure_route)
+      expect(subject).not_to receive(:remove_route)
+      subject.update_routes
+    end
+  end
 end
