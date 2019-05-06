a1e8c0093ea59b46b431d9db040e0048bfd34678
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2019-05-06 08:51:06 +0300

    fix specs

--- spec/flying_shuttle/route_service_spec.rb
+++ spec/flying_shuttle/route_service_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe FlyingShuttle::RouteService do
         10.9.145.76 dev weave scope link src 10.40.0.0
         172.31.1.1 dev eth0 scope link
       OUTPUT
-      allow(subject).to receive(:run_cmd).with('ip route').and_return(output, double(:status))
+      allow(subject).to receive(:run_cmd).with('ip route list table 10250').and_return(output, double(:status))
       addresses = subject.currently_routed_addresses
       expect(addresses).to eq(['10.9.145.75', '10.9.145.76'])
     end
